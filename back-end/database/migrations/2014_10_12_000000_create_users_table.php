90303a0bd729c50126c189c44858e61b2243835e
MakaraCoder <makaramun2@gmail.com>
2026-02-06 21:32:47 +0700

    Build auth: update users migration and add OTP migration

--- back-end/database/migrations/2014_10_12_000000_create_users_table.php
+++ back-end/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,11 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('first_name', 100);
+            $table->string('last_name', 100);
+            $table->string('role', 20);
+            $table->string('email')->unique()->nullable();
+            $table->string('phone', 20)->unique()->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('profile')->nullable();
+            $table->string('bio')->nullable();
             $table->timestamps();
         });
     }
